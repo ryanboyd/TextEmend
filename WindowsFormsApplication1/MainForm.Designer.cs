c42945083e489c8ce38b997e1a835ffe1a85d5f3
Ryan L. Boyd <ryanb@ryanb.cc>
2018-06-15 15:44:11 -0500

    v0.92
    Initial upload.

---
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -0,0 +1,205 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.BgWorker = new System.ComponentModel.BackgroundWorker();
+            this.ScanSubfolderCheckbox = new System.Windows.Forms.CheckBox();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.FolderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.FilenameLabel = new System.Windows.Forms.Label();
+            this.EncodingDropdown = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.CompactWhitespaceCheckbox = new System.Windows.Forms.CheckBox();
+            this.CaseSensitiveCheckbox = new System.Windows.Forms.CheckBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.LoadDictionaryButton = new System.Windows.Forms.Button();
+            this.saveOutputDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BgWorker
+            // 
+            this.BgWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BgWorkerClean_DoWork);
+            this.BgWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.BgWorker_RunWorkerCompleted);
+            // 
+            // ScanSubfolderCheckbox
+            // 
+            this.ScanSubfolderCheckbox.AutoSize = true;
+            this.ScanSubfolderCheckbox.Location = new System.Drawing.Point(6, 85);
+            this.ScanSubfolderCheckbox.Name = "ScanSubfolderCheckbox";
+            this.ScanSubfolderCheckbox.Size = new System.Drawing.Size(131, 20);
+            this.ScanSubfolderCheckbox.TabIndex = 2;
+            this.ScanSubfolderCheckbox.Text = "Scan subfolders?";
+            this.ScanSubfolderCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.StartButton.Location = new System.Drawing.Point(66, 241);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(152, 32);
+            this.StartButton.TabIndex = 3;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // FolderBrowser
+            // 
+            this.FolderBrowser.RootFolder = System.Environment.SpecialFolder.MyComputer;
+            this.FolderBrowser.ShowNewFolderButton = false;
+            // 
+            // FilenameLabel
+            // 
+            this.FilenameLabel.AutoEllipsis = true;
+            this.FilenameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FilenameLabel.Location = new System.Drawing.Point(12, 287);
+            this.FilenameLabel.Name = "FilenameLabel";
+            this.FilenameLabel.Size = new System.Drawing.Size(260, 15);
+            this.FilenameLabel.TabIndex = 6;
+            this.FilenameLabel.Text = "Waiting to process texts...";
+            this.FilenameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // EncodingDropdown
+            // 
+            this.EncodingDropdown.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.EncodingDropdown.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.EncodingDropdown.FormattingEnabled = true;
+            this.EncodingDropdown.Location = new System.Drawing.Point(6, 47);
+            this.EncodingDropdown.Name = "EncodingDropdown";
+            this.EncodingDropdown.Size = new System.Drawing.Size(248, 23);
+            this.EncodingDropdown.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(203, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Encoding of RegEx List && Text Files:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.CompactWhitespaceCheckbox);
+            this.groupBox2.Controls.Add(this.CaseSensitiveCheckbox);
+            this.groupBox2.Controls.Add(this.EncodingDropdown);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.ScanSubfolderCheckbox);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(260, 177);
+            this.groupBox2.TabIndex = 19;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Text Reading Controls";
+            // 
+            // CompactWhitespaceCheckbox
+            // 
+            this.CompactWhitespaceCheckbox.AutoSize = true;
+            this.CompactWhitespaceCheckbox.Checked = true;
+            this.CompactWhitespaceCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CompactWhitespaceCheckbox.Location = new System.Drawing.Point(6, 137);
+            this.CompactWhitespaceCheckbox.Name = "CompactWhitespaceCheckbox";
+            this.CompactWhitespaceCheckbox.Size = new System.Drawing.Size(156, 20);
+            this.CompactWhitespaceCheckbox.TabIndex = 12;
+            this.CompactWhitespaceCheckbox.Text = "Compact Whitespace";
+            this.CompactWhitespaceCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // CaseSensitiveCheckbox
+            // 
+            this.CaseSensitiveCheckbox.AutoSize = true;
+            this.CaseSensitiveCheckbox.Checked = true;
+            this.CaseSensitiveCheckbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.CaseSensitiveCheckbox.Location = new System.Drawing.Point(6, 111);
+            this.CaseSensitiveCheckbox.Name = "CaseSensitiveCheckbox";
+            this.CaseSensitiveCheckbox.Size = new System.Drawing.Size(181, 20);
+            this.CaseSensitiveCheckbox.TabIndex = 11;
+            this.CaseSensitiveCheckbox.Text = "Case sensitive RegExes?";
+            this.CaseSensitiveCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "RegEx List.txt";
+            this.openFileDialog.Filter = "TextEmend RegEx List|*.txt";
+            // 
+            // LoadDictionaryButton
+            // 
+            this.LoadDictionaryButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LoadDictionaryButton.Location = new System.Drawing.Point(66, 203);
+            this.LoadDictionaryButton.Name = "LoadDictionaryButton";
+            this.LoadDictionaryButton.Size = new System.Drawing.Size(152, 32);
+            this.LoadDictionaryButton.TabIndex = 20;
+            this.LoadDictionaryButton.Text = "Load RegEx List";
+            this.LoadDictionaryButton.UseVisualStyleBackColor = true;
+            this.LoadDictionaryButton.Click += new System.EventHandler(this.LoadDictionaryButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSalmon;
+            this.ClientSize = new System.Drawing.Size(284, 311);
+            this.Controls.Add(this.LoadDictionaryButton);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.FilenameLabel);
+            this.Controls.Add(this.StartButton);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(300, 350);
+            this.MinimumSize = new System.Drawing.Size(300, 350);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TextEmend";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.ComponentModel.BackgroundWorker BgWorker;
+        private System.Windows.Forms.CheckBox ScanSubfolderCheckbox;
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.FolderBrowserDialog FolderBrowser;
+        private System.Windows.Forms.Label FilenameLabel;
+        private System.Windows.Forms.ComboBox EncodingDropdown;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button LoadDictionaryButton;
+        private System.Windows.Forms.FolderBrowserDialog saveOutputDialog;
+        private System.Windows.Forms.CheckBox CaseSensitiveCheckbox;
+        private System.Windows.Forms.CheckBox CompactWhitespaceCheckbox;
+    }
+}
+
---
+++ WindowsFormsApplication1/MainForm.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+
+
+namespace WindowsFormsApplication1
+{
+
+    public partial class Form1 : Form
+    {
+
+
+        //initialize the space for our dictionary data
+        DictionaryData DictData = new DictionaryData();
+
+
+
+        //this is what runs at initialization
+        public Form1()
+        {
+
+            InitializeComponent();
+
+            foreach(var encoding in Encoding.GetEncodings())
+            {
+                EncodingDropdown.Items.Add(encoding.Name);
+            }
+
+            try
+            {
+                EncodingDropdown.SelectedIndex = EncodingDropdown.FindStringExact("utf-8");
+            }
+            catch
+            {
+                EncodingDropdown.SelectedIndex = EncodingDropdown.FindStringExact(Encoding.Default.BodyName);
+            }
+            
+
+
+        }
+
+
+
+
+
+
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+
+
+                    //make sure that our dictionary is loaded before anything else
+                    if (DictData.RegExListLoaded != true)
+                    {
+                        MessageBox.Show("You must first load a RegEx list before you can process your texts.", "RegEx list not loaded!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+            
+
+                    FolderBrowser.Description = "Please choose the location of your .txt files to analyze";
+                    if (FolderBrowser.ShowDialog() != DialogResult.Cancel) {
+
+                        DictData.TextFileFolder = FolderBrowser.SelectedPath.ToString();
+                
+                        if (DictData.TextFileFolder != "")
+                        {
+
+                            saveOutputDialog.Description = "Please choose a folder for your output files";
+                            saveOutputDialog.SelectedPath = DictData.TextFileFolder;
+                            if (saveOutputDialog.ShowDialog() != DialogResult.Cancel) {
+
+
+                                if (FolderBrowser.SelectedPath == saveOutputDialog.SelectedPath)
+                                {
+                                    MessageBox.Show("You can not use the same folder for text input and text output. If you do this, your original data would be overwritten. Please use a different folder for your output location.", "Folder selection error!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                    return;
+                                }
+
+                                DictData.OutputFileLocation = saveOutputDialog.SelectedPath;
+                                DictData.CompactWhitespace = CompactWhitespaceCheckbox.Checked;
+
+                                if (DictData.OutputFileLocation != "") {
+
+
+
+                                    StartButton.Enabled = false;
+                                    CaseSensitiveCheckbox.Enabled = false;
+                                    ScanSubfolderCheckbox.Enabled = false;
+                                    EncodingDropdown.Enabled = false;
+                                    LoadDictionaryButton.Enabled = false;
+                                    CompactWhitespaceCheckbox.Enabled = false;
+                            
+                                    BgWorker.RunWorkerAsync(DictData);
+                                }
+                            }
+                        }
+
+                    }
+
+                
+
+        }
+
+        
+
+
+
+
+        private void BgWorkerClean_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+
+            DictionaryData DictData = (DictionaryData)e.Argument;
+            DictData.NumberOfMatches = new uint[DictData.RegexArray.Length];
+            DictData.TotalFilesMatched = new uint[DictData.RegexArray.Length];
+
+            for(int i = 0; i < DictData.NumberOfMatches.Length; i++)
+            {
+                DictData.NumberOfMatches[i] = 0;
+                DictData.TotalFilesMatched[i] = 0;
+            }
+
+
+            //selects the text encoding based on user selection
+            Encoding SelectedEncoding = null;
+            this.Invoke((MethodInvoker)delegate ()
+            {
+                SelectedEncoding = Encoding.GetEncoding(EncodingDropdown.SelectedItem.ToString());
+            });
+
+            
+
+            //get the list of files
+            var SearchDepth = SearchOption.TopDirectoryOnly;
+            if (ScanSubfolderCheckbox.Checked)
+            {
+                SearchDepth = SearchOption.AllDirectories;
+            }
+            var files = Directory.EnumerateFiles(DictData.TextFileFolder, "*.txt", SearchDepth);
+
+
+
+            try {
+
+                //open up the output file
+                //using (StreamWriter outputFile = new StreamWriter(new FileStream(DictData.OutputFileLocation, FileMode.Create), SelectedEncoding))
+
+
+
+                    foreach (string fileName in files)
+                    {
+
+                        //set up our variables to report
+                        string Filename_Clean = Path.GetFileName(fileName);
+                        string Output_Location = Path.Combine(DictData.OutputFileLocation, Path.GetFileName(fileName));
+                        
+                        //report what we're working on
+                        FilenameLabel.Invoke((MethodInvoker)delegate
+                        {
+                            FilenameLabel.Text = "Processing: " + Filename_Clean;
+                        });
+
+
+
+
+                        //read in the text file, convert everything to lowercase
+                        string readText = File.ReadAllText(fileName, SelectedEncoding);
+
+                        if (DictData.CompactWhitespace) readText = Regex.Replace(readText, @"\s+", " ");
+
+
+                        //     _                _                 _____         _   
+                        //    / \   _ __   __ _| |_   _ _______  |_   _|____  _| |_ 
+                        //   / _ \ | '_ \ / _` | | | | |_  / _ \   | |/ _ \ \/ / __|
+                        //  / ___ \| | | | (_| | | |_| |/ /  __/   | |  __/>  <| |_ 
+                        // /_/   \_\_| |_|\__,_|_|\__, /___\___|   |_|\___/_/\_\\__|
+                        //                        |___/                             
+
+                        for (int i = 0; i < DictData.RegexArray.Length; i++)
+                        {
+
+                            int NumMatches = DictData.RegexArray[i].Matches(readText).Count;
+
+                            if (NumMatches == 0) continue;
+
+                            DictData.NumberOfMatches[i] += (uint)NumMatches;
+                            DictData.TotalFilesMatched[i] += 1;
+
+                            readText = DictData.RegexArray[i].Replace(readText, DictData.ReplacementArray[i]);
+
+                        }
+
+
+
+
+
+                    // __        __    _ _          ___        _               _   
+                    // \ \      / / __(_) |_ ___   / _ \ _   _| |_ _ __  _   _| |_ 
+                    //  \ \ /\ / / '__| | __/ _ \ | | | | | | | __| '_ \| | | | __|
+                    //   \ V  V /| |  | | ||  __/ | |_| | |_| | |_| |_) | |_| | |_ 
+                    //    \_/\_/ |_|  |_|\__\___|  \___/ \__,_|\__| .__/ \__,_|\__|
+                    //                                            |_|              
+
+                    //open up the output file
+                    using (StreamWriter outputFile = new StreamWriter(new FileStream(Output_Location, FileMode.Create), SelectedEncoding))
+                    {
+                        outputFile.Write(readText);
+                    }
+
+
+                }
+
+
+                using (StreamWriter outputFile = new StreamWriter(new FileStream(Path.Combine(DictData.OutputFileLocation, "__TextEmend-Report.csv"), FileMode.Create), SelectedEncoding))
+                {
+                    outputFile.WriteLine("\"RegEx\",\"Replacement\",\"NumberOfMatches\",\"FilesWithPattern\"");
+
+                    for (int i = 0; i < DictData.RegexArray.Length; i++)
+                    {
+                        outputFile.WriteLine("\"" + DictData.RegexArray[i].ToString() + "\"," +
+                                             "\"" + DictData.ReplacementArray[i] + "\"," + 
+                                             DictData.NumberOfMatches[i].ToString() + "," + 
+                                             DictData.TotalFilesMatched[i].ToString());
+                    }
+
+                    
+
+                }
+
+
+
+            }
+            catch
+            {
+                MessageBox.Show("TextEmend encountered an issue somewhere while trying to analyze your texts. The most common cause of this is trying to open your output file(s) while the program is still running. Did any of your input files move, or is your output file being opened/modified by another application? Are you sure that your regular expressions are properly formed?", "Error while analyzing", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            
+        }
+
+
+        //when the bgworker is done running, we want to re-enable user controls and let them know that it's finished
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            StartButton.Enabled = true;
+            ScanSubfolderCheckbox.Enabled = true;
+            EncodingDropdown.Enabled = true;
+            LoadDictionaryButton.Enabled = true;
+            CaseSensitiveCheckbox.Enabled = true;
+            CompactWhitespaceCheckbox.Enabled = true;
+            FilenameLabel.Text = "Finished!";
+            MessageBox.Show("TextEmend has finished processing your texts.", "Analysis Complete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+
+
+
+
+        public class DictionaryData
+        {
+
+            public string TextFileFolder { get; set; }
+            public string OutputFileLocation { get; set; }
+
+
+            public Regex[] RegexArray { get; set; }
+            public string[] ReplacementArray { get; set; }
+
+            public uint[] NumberOfMatches { get; set; }
+            public uint[] TotalFilesMatched { get; set; }
+
+            public bool RegExListLoaded { get; set; } = false;
+            public bool CompactWhitespace { get; set; }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        private void LoadDictionaryButton_Click(object sender, EventArgs e)
+        {
+
+
+            
+            DictData = new DictionaryData();
+
+
+           //   ____                _   _____         _     _____                        ____ ___ ____   _____ _ _      
+           //  |  _ \ ___  __ _  __| | |_   _|____  _| |_  |  ___| __ ___  _ __ ___     |  _ \_ _/ ___| |  ___(_) | ___ 
+           //  | |_) / _ \/ _` |/ _` |   | |/ _ \ \/ / __| | |_ | '__/ _ \| '_ ` _ \    | | | | | |     | |_  | | |/ _ \
+           //  |  _ <  __/ (_| | (_| |   | |  __/>  <| |_  |  _|| | | (_) | | | | | |  _| |_| | | |___  |  _| | | |  __/
+           //  |_| \_\___|\__,_|\__,_|   |_|\___/_/\_\\__| |_|  |_|  \___/|_| |_| |_| (_)____/___\____| |_|   |_|_|\___|
+                                                                                                          
+
+
+
+            string RegexListFileRawText = "";
+
+            openFileDialog.Title = "Please choose your TextEmend RegEx List file";
+
+            if (openFileDialog.ShowDialog() != DialogResult.Cancel)
+            {
+
+                FolderBrowser.SelectedPath = System.IO.Path.GetDirectoryName(openFileDialog.FileName);
+
+                //Load dictionary file now
+                try
+                {
+
+
+                    Encoding SelectedEncoding = null;
+                    SelectedEncoding = Encoding.GetEncoding(EncodingDropdown.SelectedItem.ToString());
+
+                    RegexListFileRawText = File.ReadAllText(openFileDialog.FileName, SelectedEncoding);
+                }
+                catch
+                {
+                    MessageBox.Show("TextEmend is having trouble reading data from your RegEx list file. Is it open in another application?");
+                    return;
+                }
+
+            }
+            else
+            {
+                return;
+            }
+
+
+
+            //  ____                   _       _         ____  _      _   ____        _           ___  _     _           _   
+            // |  _ \ ___  _ __  _   _| | __ _| |_ ___  |  _ \(_) ___| |_|  _ \  __ _| |_ __ _   / _ \| |__ (_) ___  ___| |_ 
+            // | |_) / _ \| '_ \| | | | |/ _` | __/ _ \ | | | | |/ __| __| | | |/ _` | __/ _` | | | | | '_ \| |/ _ \/ __| __|
+            // |  __/ (_) | |_) | |_| | | (_| | ||  __/ | |_| | | (__| |_| |_| | (_| | || (_| | | |_| | |_) | |  __/ (__| |_ 
+            // |_|   \___/| .__/ \__,_|_|\__,_|\__\___| |____/|_|\___|\__|____/ \__,_|\__\__,_|  \___/|_.__// |\___|\___|\__|
+            //            |_|                                                                             |__/               
+
+
+
+
+            //parse out the the dictionary file
+
+            List<Regex> RegexList = new List<Regex>();
+            List<string> ReplacementList = new List<string>();
+
+
+            try
+            {
+                string[] EntryLines = RegexListFileRawText.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                //Map out the input entries
+                for (int i = 0; i < EntryLines.Length; i++)
+                {
+
+                    string[] EntryRow = EntryLines[i].Split(new char[] { '\t' });
+
+
+                    if (CaseSensitiveCheckbox.Checked) { 
+                        RegexList.Add(new Regex(@EntryRow[0], RegexOptions.Compiled));
+                    }
+                    else
+                    {
+                        RegexList.Add(new Regex(@EntryRow[0], RegexOptions.Compiled | RegexOptions.IgnoreCase));
+                    }
+                    ReplacementList.Add(EntryRow[1]);
+
+                }
+
+
+                DictData.RegexArray = RegexList.ToArray();
+                DictData.ReplacementArray = ReplacementList.ToArray();
+
+                DictData.RegExListLoaded = true;
+
+                MessageBox.Show("Your regular expression list has been successfully loaded.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch
+            {
+                MessageBox.Show("TextEmend encountered an error while trying to parse out your RegEx list. Please check to make sure that it is correctly formatted, that your regular expressions are valid, and that your list file is not currently open in another application.", "Error parsing RegEx list", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                DictData = new DictionaryData();
+                return;
+            }
+
+
+    }
+
+
+
+    }
+    
+
+
+}
